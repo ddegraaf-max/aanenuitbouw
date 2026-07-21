a68bb3f31bddee925124978dd048181f36e5583d
ddegraaf-max <d.degraaf@creditline.nl>
2026-07-21 18:29:44 +0200

    nkgjfhrds

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const MIME_TYPES = {
   '.woff':  'font/woff',
   '.woff2': 'font/woff2',
   '.txt':  'text/plain; charset=utf-8',
+  '.xml':  'application/xml; charset=utf-8',
 };
 
 function jsonResponse(res, status, payload) {
